768f596d091c1a40a579c673bd4e6c2f7e7bd30a
Can Gökçeaslan <48398625+cangokceaslan@users.noreply.github.com>
2020-11-14 18:42:27 +0300

    v3.0
    v3.0

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+const getParams = function (url) {
+    var params = {};
+    var parser = document.createElement('a');
+    parser.href = url;
+    var query = parser.search.substring(1);
+    var vars = query.split('&');
+    for (var i = 0; i < vars.length; i++) {
+        var pair = vars[i].split('=');
+        params[pair[0]] = decodeURIComponent(pair[1]);
+    }
+    return params;
+};
+
+var urlStr = window.location.href;
+var params = getParams(urlStr);
+var urlElement = document.getElementById("url");
+var url2Element = document.getElementById("url2");
+urlElement.href = params.url;
+url2Element.href = params.url;
